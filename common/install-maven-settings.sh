be464b9819a9ca5d5568855aee113e9dc581faf5
unknown <christian@qxx.se>
2017-08-18 20:57:45 +0200

    fixed issue

--- common/install-maven-settings.sh
+++ common/install-maven-settings.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 if [ -f /vagrant_common/maven_settings.xml ]; then
+   if [ -d /home/vagrant/.m2 ]; then
+       mkdir /home/vagrant/.m2
+   fi
+   
    cp /vagrant_common/maven_settings.xml /home/vagrant/.m2/settings.xml
 fi
