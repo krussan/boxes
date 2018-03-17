6f4a3cb6acd83ee6ef79e84e424ef8b804c27e22
Christian Thunström <christian@qxx.se>
2018-03-17 21:19:49 +0100

    Fixed yum command

--- common/init.sh
+++ common/init.sh
@@ -3,7 +3,7 @@ yum update -y --skip-broken
 localectl set-keymap se
 localectl set-x11-keymap se
 
-yum install wget,expect,dos2unix -y
+yum install wget expect dos2unix -y
 #yum -y groups install "GNOME Desktop"
 
 dos2unix /vagrant/*.sh
